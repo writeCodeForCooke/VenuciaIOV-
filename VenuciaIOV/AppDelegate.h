933d95ecac0a89269170dd05c8fc1616d35ab684
DevCharlesLee <iosdevli@163.com>
2016-12-14 15:16:41 +0800

    Initial Commit

---
+++ VenuciaIOV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VenuciaIOV
+//
+//  Created by li on 2016/12/14.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VenuciaIOV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VenuciaIOV
+//
+//  Created by li on 2016/12/14.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
